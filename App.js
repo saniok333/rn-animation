29700a3770f37445b677aee87b7628c9de664054
saniok333 <alex.o.csc@gmail.com>
2022-09-05 17:59:53 +0300

    Consecutive opacity

--- App.js
+++ App.js
@@ -2,40 +2,62 @@ import { useRef, useEffect } from 'react';
 import { StyleSheet, Animated, Easing } from 'react-native';
 
 export default function App() {
-  const translation = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
+  const firstOpacity = useRef(new Animated.Value(0)).current;
+  const secondOpacity = useRef(new Animated.Value(0)).current;
+  const thirdOpacity = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
-    Animated.parallel([
-      Animated.timing(translation.x, {
-        toValue: 250,
+    Animated.stagger(1000, [
+      Animated.timing(firstOpacity, {
+        toValue: 1,
         useNativeDriver: true,
       }),
-      Animated.timing(translation.y, {
-        toValue: 250,
+      Animated.timing(secondOpacity, {
+        toValue: 1,
+        useNativeDriver: true,
+      }),
+      Animated.timing(thirdOpacity, {
+        toValue: 1,
         useNativeDriver: true,
       }),
     ]).start();
   }, []);
 
   return (
-    <Animated.View
-      style={[
-        styles.container,
-        {
-          transform: [
-            { translateX: translation.x },
-            { translateY: translation.y },
-          ],
-        },
-      ]}
-    ></Animated.View>
+    <>
+      <Animated.View
+        style={[
+          styles.container,
+          {
+            opacity: firstOpacity,
+          },
+        ]}
+      ></Animated.View>
+      <Animated.View
+        style={[
+          styles.container,
+          {
+            opacity: secondOpacity,
+          },
+        ]}
+      ></Animated.View>
+      <Animated.View
+        style={[
+          styles.container,
+          {
+            opacity: thirdOpacity,
+          },
+        ]}
+      ></Animated.View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    margin: 100,
     width: 100,
     height: 100,
-    backgroundColor: 'yellow',
+    backgroundColor: 'orange',
   },
 });
