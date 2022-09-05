6e9bbabd5dc4bd0acb7c8198ffb79ea895e86e3a
saniok333 <alex.o.csc@gmail.com>
2022-09-05 15:46:41 +0300

    The Basics of React Native animations

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef, useEffect } from 'react';
+import { StyleSheet, Animated } from 'react-native';
 
 export default function App() {
+  const translation = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(translation, {
+      toValue: 250,
+      useNativeDriver: true,
+    }).start();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Animated.View
+      style={[styles.container, { transform: [{ translateX: translation }] }]}
+    ></Animated.View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: 100,
+    height: 100,
+    backgroundColor: 'yellow',
   },
 });
