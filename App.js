3c7f995398d65ebf96b4b22cd7d04bbb5dad7998
saniok333 <alex.o.csc@gmail.com>
2022-09-05 16:57:42 +0300

    Animated sequence

--- App.js
+++ App.js
@@ -2,21 +2,32 @@ import { useRef, useEffect } from 'react';
 import { StyleSheet, Animated, Easing } from 'react-native';
 
 export default function App() {
-  const translation = useRef(new Animated.Value(0)).current;
+  const translation = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
 
   useEffect(() => {
-    Animated.timing(translation, {
-      toValue: 250,
-      delay: 2000,
-      duration: 3000,
-      easing: Easing.bounce,
-      useNativeDriver: true,
-    }).start();
+    Animated.sequence([
+      Animated.timing(translation.x, {
+        toValue: 250,
+        useNativeDriver: true,
+      }),
+      Animated.timing(translation.y, {
+        toValue: 250,
+        useNativeDriver: true,
+      }),
+    ]).start();
   }, []);
 
   return (
     <Animated.View
-      style={[styles.container, { transform: [{ translateX: translation }] }]}
+      style={[
+        styles.container,
+        {
+          transform: [
+            { translateX: translation.x },
+            { translateY: translation.y },
+          ],
+        },
+      ]}
     ></Animated.View>
   );
 }
